4807ee485d4b2c80954ad3882d9bcede0924d529
Vivek Vajipey <vivek.vajipey@gmail.com>
2025-04-11 11:49:43 -0700

    back to camera screen

--- frontend-worlddex/app/index.tsx
+++ frontend-worlddex/app/index.tsx
@@ -1,10 +1,5 @@
-import { View } from 'react-native';
-import { TestPhotoUpload } from '../src/components/TestPhotoUpload';
+import CameraScreen from "./(screens)/camera";
 
-export default function App() {
-  return (
-    <View className="flex-1 items-center justify-center">
-      <TestPhotoUpload />
-    </View>
-  );
+export default function Index() {
+  return <CameraScreen />;
 }
