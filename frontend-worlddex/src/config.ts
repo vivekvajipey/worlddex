342fcd67e12f7db58d2d28440fcb2bda88f8b354
Jaiden Reddy <jaidenreddy@gmail.com>
2025-04-17 13:46:58 -0700

    fly setup

--- frontend-worlddex/src/config.ts
+++ frontend-worlddex/src/config.ts
@@ -6,8 +6,8 @@ declare const __DEV__: boolean;
 // In development, use localhost
 export const API_URL = __DEV__
   ? // ? 'http://10.0.0.195:3000/api'
-    "http://10.27.183.186:3000/api" // Stanford
-  : "https://your-api-url.com/api"; // TODO: Update with real URL
+    "http://10.31.147.38:3000/api" // Stanford
+  : "https://backend-worlddex.fly.dev/api"; // Production URL
 
 // Helper to check if we're running in Expo Go
 export const isExpoGo = Constants.appOwnership === "expo";
